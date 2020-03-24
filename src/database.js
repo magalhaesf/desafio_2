1490c7498918208715ed83d6760ab8d6758aaacb
Felipe Magalhães <50468162+magalhaesf@users.noreply.github.com>
2020-03-23 23:46:23 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/plathanus';
+const URI = 'mongodb+srv://ptn:ptn123@cluster0-gfimd.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(URI)
     .then(db => console.log('Banco conectado'))
